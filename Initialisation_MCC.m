6dab07a559ea2a82e97392f7373d8d03615ae7ec
SanjaiRavi <61349436+SanjaiRavi@users.noreply.github.com>
2021-01-20 12:48:44 +0100

    Add files via upload

---
+++ Initialisation_MCC.m
@@ -0,0 +1,6 @@
+%Machine parameters
+L=1e-1;%Motor self-inductance
+R=50;%Internal resistance
+J=1e-2;%Inertia of motor arm 
+f=1e-1;%Friction coefficient
+k=0.05;%Electromechanical coupling coefficient
